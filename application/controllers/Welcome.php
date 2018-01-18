ad9234c3d27d37a853af528d934427df7f08c5d9
Alfred <alfredswinton@gmail.com>
2018-01-18 15:07:26 -0800

    Added actor.php fixed welcome.php to enable individual actor pages

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,6 +25,21 @@ class Welcome extends Application
 		$this->data['authors'] = $source;
 
 		$this->render();
-	}
+
+  }
+
+    public function show($key)
+    {
+      // this is the view we want shown
+      $this->data['pagebody'] = 'actor';
+
+      // build the list of authors, to pass on to our view
+      $source = $this->quotes->get($key);
+
+      // pass on the data to present, as the "authors" view parameter
+      $this->data = array_merge($this->data, (array) $source);
+
+      $this->render();
+    } 
 
 }
--- changelog.md
+++ changelog.md
@@ -8,6 +8,7 @@ Convention
 Allman Notation
 
 Changelog (newest at the top)
+Alfred 2018-01-18 15:05 - Added individual actor portion code to welcome and actor.php
 Haley  2018-01-18 14:40 - Added tooltips showing authors name and role
 Alfred 2018-01-18 14:29 - Added an image and a quote
 Haley 2018-01-18 13:59 - Created the changelog
